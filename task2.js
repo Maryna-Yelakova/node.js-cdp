d59e4c2c19f13a2e909c929a6f8373b73812701f
Maryna_Yelakova <Maryna_Yelakova@epam.com>
2019-12-08 23:47:39 +0200

    add babel

--- task2.js
+++ task2.js
@@ -1,5 +1,5 @@
-const csv = require('csvtojson');
-const fs = require('fs');
+import csv from 'csvtojson';
+import fs from 'fs';
 
 const csvFilePath ='./csv/node_mentoring_t1_2_input_example.csv';
 
